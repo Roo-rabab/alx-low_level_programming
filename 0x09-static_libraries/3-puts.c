ecd94baff21f7a9cd403c1abbd0e0109bc7b231e
Roo-rabab <rababmchich312@gmail.com>
2023-05-12 08:06:25 -0700

    mv

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,23 +1,13 @@
 #include "main.h"
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
- *
- * Return: None.
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
 void _puts(char *str)
 {
-    /* Implementation of the _puts function */
-}
-
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
