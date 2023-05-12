5637ebb7631ae281ebec7c29e8c1d78879cbb048
Roo-rabab <rababmchich312@gmail.com>
2023-05-12 06:55:54 -0700

    task 1

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Return: None.
+ */
+void _puts(char *str)
+{
+    /* Implementation of the _puts function */
+}
+
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
